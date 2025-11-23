bba1dd847764385ef24e1a09a99d0464491f4c2a
mamamobluee699-lab <mamamobluee699@gmail.com>
2025-11-24 02:54:55 +0800

    Remove problematic middleware

--- myproject/settings.py
+++ myproject/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'myapp.middleware.AdminSessionTimeoutMiddleware',
 ]
 
 # ============================================
